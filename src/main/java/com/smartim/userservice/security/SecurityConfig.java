509db905aea32c7c46c43fe448c890332f8cb079
viveknikam <viveknikam339@gmail.com>
2025-06-03 22:36:16 +0530

    Updating user service

---
+++ src/main/java/com/smartim/userservice/security/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.smartim.userservice.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+}
