0f812aaf2a58a96a7f3437e84f6619a3dc6629ad
viveknikam <viveknikam339@gmail.com>
2025-07-05 22:55:49 +0530

    Updating user service

--- src/main/java/com/smartim/userservice/SmartimUserServiceApplication.java
+++ src/main/java/com/smartim/userservice/SmartimUserServiceApplication.java
@@ -1,9 +1,33 @@
 package com.smartim.userservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info=@Info(
+				title = "User microservice REST API Documentation",
+				description = "SMARTIM User microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Vivek Nikam",
+						email = "viveknikam@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = " "
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "SMARTIM User microservice REST API Documentation",
+				url = " "
+		)
+)
 public class SmartimUserServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/smartim/userservice/dto/UserDto.java
+++ src/main/java/com/smartim/userservice/dto/UserDto.java
@@ -1,31 +1,66 @@
 package com.smartim.userservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import java.time.LocalDateTime;
 
 /**
  * ResponseDto DTO used to return user details
  */
+@Schema(
+        name = "User",
+        description = "Schema to hold user information"
+)
 @Data
 public class UserDto {
 
+    @Schema(
+            description = "Email of the user", example = "xyx@xyz.com"
+    )
     private String email;
 
+    @Schema(
+            description = "Full name of the user", example = "Vivek Nikam"
+    )
     private String fullName;
 
+    @Schema(
+            description = "Mobile number of the user", example = "1234567890"
+    )
     private String mobileNumber;
 
+    @Schema(
+            description = "User status of the user", example = "true"
+    )
     private Boolean userStatus;
 
+    @Schema(
+            description = "Role of the user", example = "ADMIN"
+    )
     private String role;
 
+    @Schema(
+            description = "User name of the user", example = "vnvivek"
+    )
     private String userName;
 
+    @Schema(
+            description = "Date on which user got registered", example = "2025-07-05T19:21:33.727138"
+    )
     private LocalDateTime createdOn;
 
+    @Schema(
+            description = "User name of the person by whom user got registered", example = "vnvivek"
+    )
     private String createdBy;
 
+    @Schema(
+            description = "Date on which user got updated", example = "2025-07-05T19:21:33.727138"
+    )
     private LocalDateTime updatedOn;
 
+    @Schema(
+            description = "User name of the person by whom user got updated", example = "vnvivek"
+    )
     private String updatedBy;
 }
