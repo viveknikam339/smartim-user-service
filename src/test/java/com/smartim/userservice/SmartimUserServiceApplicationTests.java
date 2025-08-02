7c3ebd3eb8dc5623283d3a1d0e4e4b63eba81bbc
viveknikam <viveknikam339@gmail.com>
2025-08-02 16:45:52 +0530

    Updating user service

--- src/test/java/com/smartim/userservice/SmartimUserServiceApplicationTests.java
+++ src/test/java/com/smartim/userservice/SmartimUserServiceApplicationTests.java
@@ -1,13 +1,11 @@
 package com.smartim.userservice;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SmartimUserServiceApplicationTests {
 
-	@Disabled
 	@Test
 	void contextLoads() {
 		//Loads context
